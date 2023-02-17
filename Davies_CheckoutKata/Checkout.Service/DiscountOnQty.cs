06f8ddbcc0c4dd5f8e8fc6569acf5426e35c85d2
Dhori Sindhu <dhori.sindhu@gmail.com>
2023-02-17 13:44:55 +0000

    STEP 2 Created Products

---
+++ Davies_CheckoutKata/Checkout.Service/DiscountOnQty.cs
@@ -0,0 +1,9 @@
+﻿namespace Checkout.Service
+{
+    public class DiscountOnQty
+    {
+        public char SKU { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
